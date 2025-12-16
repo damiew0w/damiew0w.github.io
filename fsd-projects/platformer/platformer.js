05407c631e43302ef54f97e7b4d13ff0c2181b10
damiew0w <damien.breecher@jpschools.org>
2025-12-16 19:16:17 +0000

    message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -1,31 +1,24 @@
-   toggleGrid();
-
-
-
-
-   // TODO 2 - Create Platforms
-
-
-   createPlatform(400, 600, 110, 45);
-   createPlatform(300, 520, 110, 45);
-   createPlatform(500, 420, 110, 45);
-   createPlatform(510, 600, 110, 45);
-   createPlatform(700, 420, 110, 45);
-   createPlatform(900, 600, 110, 45);
-
-
-
-
-
-
-   // TODO 3 - Create Collectables
-
-
-  createCollectable("steve", 500, 400);
-  createCollectable("diamond", 200, 170, 0.5, 0.7);
-
-
-
-     createCannon("top", 200, 1000);
-   createCannon("right", 300, 1000);
-   createCannon("top", 450, 1000);
+   
+// TODO 1 - Enable the Grid
+    toggleGrid();
+    // TODO 2 - Create Platforms
+    createPlatform(380, 620, 270, 20);
+    createPlatform(380, 640, 20, 100);
+    createPlatform(700, 510, 150, 20);
+    createPlatform(600, 380, 150, 20);
+    createPlatform(300, 300, 300, 20);
+    createPlatform(100, 180, 200, 20);
+    createPlatform(900, 280, 200, 20);
+    createPlatform(1225, 500, 50, 150);
+    createPlatform(1250, 630, 200, 20);
+    // TODO 3 - Create Collectables
+    createCollectable("diamond", 450, 700);
+    createCollectable("steve", 650, 350);
+    createCollectable("max", 1350, 600);
+    createCollectable("database", 150, 150);
+    // TODO 4 - Create Cannons
+    createCannon("left", 540, 2500);
+    createCannon("right", 760, 2250);
+    createCannon("top", 860, 1550);
+    createCannon("left", 190, 1500);
+    createCannon("top", 1390, 2700);
